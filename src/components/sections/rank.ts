646dfc4f48a7e026596d707be514ad88f83dc568
endlessdev <ysw0094@gmail.com>
2017-06-03 14:35:30 +0900

    Add function for refresh ranks data

--- src/components/sections/rank.ts
+++ src/components/sections/rank.ts
@@ -9,27 +9,44 @@ export default class SectionRank extends Vue {
 
     name = 'section-rank';
     rankResult: RankResult;
+    isInitialDataLoaded: boolean = false;
     isDataLoaded: boolean = false;
+    dataLoadedSecond: number = 0;
+
+    intervalId: number;
+
 
     async created() {
-       this.updateRankData();
+        await this.updateRankData();
     }
 
-    async updateRankData(){
+    @vts.watch('rankResult')
+    async updateRankData() {
+        this.isDataLoaded = false;
         let rankResponse = await axios.get(`http://localhost:3000/rank/all`);
-
         switch (rankResponse.status) {
             case 200:
 
                 for (let data in rankResponse.data) {
                     if (rankResponse.data.hasOwnProperty(data)) {
-                        rankResponse.data[data] = rankResponse.data[data].filter((element, idx) => {
+                        rankResponse.data[data] = await rankResponse.data[data].filter((element, idx) => {
                             return idx < 10;
                         })
                     }
                 }
 
+                this.dataLoadedSecond = 0;
+
+                if(this.intervalId){
+                    clearInterval(this.intervalId);
+                }
+
+                this.intervalId = setInterval(() => {
+                    this.dataLoadedSecond++;
+                }, 1000);
+
                 this.rankResult = rankResponse.data;
+                this.isInitialDataLoaded = true;
                 this.isDataLoaded = true;
 
                 break;
--- src/components/sections/rank.vue
+++ src/components/sections/rank.vue
@@ -9,8 +9,8 @@
                     <h1 class="title">실시간 통합 인기검색어</h1>
                     <p class="summary"><b>총 4개의 포털사이트에서 상시로 실시간 인기검색어를 크롤합니다.</b>
                         <br>
-                        최근 1분전 업데이트 됨
-                        <button class="ui basic small button" v-on:click="updateRankData()">
+                        {{dataLoadedSecond}}초 전 업데이트 됨
+                        <button class="ui basic small button" v-bind:class="{ loading: isDataLoaded==false }" v-on:click="updateRankData()">
                             <i class="icon refresh"></i>
                             새로고침
                         </button>
@@ -18,16 +18,16 @@
                 </div>
                 <div class="ui stackable four column grid container now-content-grid">
                     <div class="column ranking-column naver">
-                        <rank-list type="naver" :rankData="rankResult.naver" v-if="isDataLoaded"></rank-list>
+                        <rank-list type="naver" :rankData="rankResult.naver" v-if="isInitialDataLoaded"></rank-list>
                     </div>
                     <div class="column ranking-column daum">
-                        <rank-list type="daum" :rankData="rankResult.daum" v-if="isDataLoaded"></rank-list>
+                        <rank-list type="daum" :rankData="rankResult.daum" v-if="isInitialDataLoaded"></rank-list>
                     </div>
                     <div class="column ranking-column nate">
-                        <rank-list type="nate" :rankData="rankResult.nate" v-if="isDataLoaded"></rank-list>
+                        <rank-list type="nate" :rankData="rankResult.nate" v-if="isInitialDataLoaded"></rank-list>
                     </div>
                     <div class="column ranking-column zum">
-                        <rank-list type="zum" :rankData="rankResult.zum" v-if="isDataLoaded"></rank-list>
+                        <rank-list type="zum" :rankData="rankResult.zum" v-if="isInitialDataLoaded"></rank-list>
                     </div>
                 </div>
             </div>
