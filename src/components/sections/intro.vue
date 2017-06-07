ef46ef88b9b14105ac61828ad164fc2ca83faa53
endlessdev <ysw0094@gmail.com>
2017-06-07 22:11:52 +0900

    Missing type error

--- src/components/sections/intro.vue
+++ src/components/sections/intro.vue
@@ -4,7 +4,7 @@
             <div class="ui stackable two column grid container top-content-grid">
                 <div class="column intro-column">
                     <div class="intro-column-wrapper">
-                        <h1 class="title"><b>The Intergration of</b><br><span class="typing-text"></span></h1>
+                        <h1 class="title"><b>The Integration of</b><br><span class="typing-text"></span></h1>
                         <p class="summary">실시간으로 여러 포털사이트의 검색 동향을 한눈에 살펴보세요.<br>더 이상 한 포털사이트에 의존하지 않아도 됩니다.</p>
                         <button class="ui button icon large teal"><i class="arrow right icon"></i> 지금 한 눈에 보기</button>
                     </div>
