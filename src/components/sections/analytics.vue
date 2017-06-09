8f9f1cc14e9b81c2fc287ae3bb9faffb53098e62
endlessdev <ysw0094@gmail.com>
2017-06-09 15:12:26 +0900

    Add loader to analytics chart

--- src/components/sections/analytics.vue
+++ src/components/sections/analytics.vue
@@ -1,6 +1,7 @@
 <template>
     <section class="analytics-content">
         <div class="ui container chart-content-wrapper">
+            <div class="ui loader text active" v-if="!isInitialDataLoaded">차트 데이터를 불러오는 중입니다.</div>
             <analytics-chart :height="200" :data="chartData"
                          v-if="isInitialDataLoaded"></analytics-chart>
         </div>
