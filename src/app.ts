54427535b535dbe2e65d7ce732ea30729030b939
endlessdev <ysw0094@gmail.com>
2017-06-09 15:15:18 +0900

    Remove not used imported variable

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,7 @@ import * as vts from 'vue-typescript-component'
 import * as LayoutHeader from './components/layout/header.vue'
 import * as LayoutFooter from './components/layout/footer.vue'
 
-import * as index from './pages/index.vue';
-import * as keyword from './pages/keyword.vue';
-
-
-@vts.component({components: {LayoutFooter, LayoutHeader, index, keyword}})
+@vts.component({components: {LayoutFooter, LayoutHeader}})
 export default class App extends Vue {
 
 }
